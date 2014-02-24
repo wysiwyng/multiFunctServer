19d6de1c539c92db46972386b426f8ca6ba628ef
wysiwyng <wysiwyng@turbine.local>
2014-02-24 19:43:03 +0100

    edited ServerClient.cs

--- serverExternals/ServerClient.cs
+++ serverExternals/ServerClient.cs
@@ -4,8 +4,30 @@ namespace serverExternals
 {
 	public class ServerClient
 	{
-		public ServerClient ()
+		public int ClientID { get; set; }
+
+		public byte[] Address { get; set; }
+
+		public ServerClient()
 		{
+			Address = new byte[4];
+		}
+
+		public ServerClient(int id)
+		{
+			Address = new byte[4];
+			ClientID = id;
+		}
+
+		public ServerClient(int id, byte[] ip)
+		{
+			Address = ip;
+			ClientID = id;
+		}
+
+		public override string ToString()
+		{
+			return "";
 		}
 	}
 }
